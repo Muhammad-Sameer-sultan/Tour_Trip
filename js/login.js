61037ffa1081ad9931872b3d5c34ed046a3c346a
sameer <sameersultan1001@gmail.com>
2023-06-22 16:30:45 +0500

    signup javascript complete

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const user_icon_name= document.querySelector("#user-icon-name");
+
+
+window.onload =function(){
+    
+    let login_user= localStorage.getItem('user_storages');
+    console.log(login_user);
+    // user_icon_name.innerHTML=login_user.user_name;
+
+}
--- js/signpage.js
+++ js/signpage.js
@@ -8,7 +8,10 @@ let inlineRadio2 = document.querySelector('#inlineRadio2');
 let signup_password = document.querySelector('#signup-password');
 let signup_confirm_password = document.querySelector('#signup-confirm-password');
 let signup_btn = document.querySelector('#signup-btn');
+let signin_btn = document.querySelector('#signin-btn');
 let signup_link = document.querySelector('#signup_link');
+let form_box = document.querySelector('.form-box');
+let title_box = document.querySelector('.title-box');
 
 
 signup_btn.addEventListener('click', function (e) {
@@ -57,7 +60,12 @@ signup_btn.addEventListener('click', function (e) {
         localStorage.setItem('user_storages',user_storage);
         show('Success','your Account is created Suceess');
         document.querySelector('.alert').classList.replace('alert-danger','alert-success');
-        window.location.href = "sign_in page.html";
+
+        setTimeout(function () {
+            window.location.href = "sign_in page.html";
+            
+        }, 1000)
+        
     }
 
 
@@ -81,8 +89,58 @@ function show(type, message) {
 }
 //sign up link
 
+let inner= document.querySelectorAll('.inner');
 
 signup_link.addEventListener('click',function(e){
-        signup_btn.classList.toggle('d-block');
-        inner.classList.toggle('d-none');
+        // signin_title.classList.toggle('d-block');
+        // signup_title.classList.toggle('d-none');
+        title_box.classList.toggle('order-1');
+        title_box.classList.replace('rounded-start-5','rounded-end-5')
+        form_box.classList.replace('rounded-end-5','rounded-start-5')
+
+
+
+        inner.forEach(function(element) {
+            element.classList.toggle('d-none');
+            // element.classList.toggle('d-none');
+        })
+
+        
+        
 });
+
+// SIGN IN BUTTON CLICK
+
+     signin_btn.addEventListener('click',function(e){
+        e.preventDefault();
+
+        let signin_email=document.querySelector('#signin-email');
+        let signin_password=document.querySelector('#signin-password');
+        let user_icon_name=document.querySelector('#user-icon-name');
+
+        
+
+        let user_storage = localStorage.getItem('user_storages'); 
+        
+        let user_list = JSON.parse(user_storage);
+        let login_user;
+
+        let crediential_check= user_list.find(userfind=>
+            {
+                login_user=userfind;
+                return userfind.user_email===signin_email.value && userfind.user_passwod===signin_password.value;   
+            })
+            
+            if(crediential_check){
+console.log(login_user);
+                localStorage.setItem('login_storage',"login_user")
+                window.location.href='index.html';
+                // user_icon_name.innerHTML=login_user.user_name;
+
+                
+            }
+            else{
+                show('Invalid', 'Email and password are not correct')
+            }
+
+     })
