ac7a7b6590c9d021f589c9b8cae292d64f98c60a
Sameer <sameersultan1001@gmail.com>
2023-06-23 23:40:14 +0500

    javascript complete for profile and login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,23 @@
 const user_icon_name= document.querySelector("#user-icon-name");
+const home_signin_btn= document.querySelector("#home-signin-btn");
+const home_profile_container= document.querySelector("#home-profile-container");
+const logout_btn= document.querySelector("#logout_btn");
 
 
 window.onload =function(){
     
     let login_user= JSON.parse(localStorage.getItem('login_storage'));
-    console.log(login_user);
-    user_icon_name.innerHTML=login_user.user_name;
+    if(login_user!==null){
+        
+        user_icon_name.innerHTML=login_user.user_name;
+        home_signin_btn.classList.toggle('d-none');
+        home_profile_container.classList.toggle('d-none');
+        
+    }
+    
+    logout_btn.addEventListener('click',function(e){
+        console.log(login_user);
+        localStorage.removeItem("login_storage");
+    })
 
 }
--- js/profile.js
+++ js/profile.js
@@ -2,15 +2,24 @@ let p_email = document.querySelector('#p-email');
 let p_name = document.querySelector('#p-name');
 let p_dob = document.querySelector('#p-dob');
 let p_gender = document.querySelector('#p-gender');
+let user_icon_name = document.querySelector('#user-icon-name');
 
 
 window.onload=function () {
 
     let login_user = JSON.parse(localStorage.getItem('login_storage'));
 
-
+if(login_user!==null){
+console.log(p_email);
+    user_icon_name.innerHTML = login_user.user_name;
     p_email.innerHTML = login_user.user_email;
     p_name.innerHTML = login_user.user_name;
     p_dob.innerHTML = login_user.user_dob;
     p_gender.innerHTML = login_user.user_gender;
+    
+}
+
+logout_btn.addEventListener('click',function(e){
+    localStorage.removeItem("login_storage");
+})
 }
